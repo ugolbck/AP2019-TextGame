8b38b0002225bc8fe52e027b7465ad46f48145f0
Ugo Loobuyck <ugo813@hotmail.fr>
2019-04-01 10:23:14 +0200

    Instantiate items/doors or make a list of them inside Room class?

--- textGame.py
+++ textGame.py
@@ -3,7 +3,7 @@ from collections import deque
 
 
 class Character:
-    """ Class that holds information on the player """
+    ''' Class that holds information on the player '''
     def __init__(self):
         self._position = ''
         self._inventory = deque(maxlen=2)
@@ -19,7 +19,7 @@ class Character:
 
 
 class Item:
-    """ Class that defines items """
+    ''' Class that defines items '''
     def __init__(self, i_name, i_position, i_type, i_function=None):
         self._name = i_name
         self._position = i_position
@@ -28,18 +28,26 @@ class Item:
 
 
 class House:
-    """ Class that defines the house and holds its components """
-    def __init__(self, file):
-        self.filename = file
-        self._rooms
+    ''' Class that defines the house and holds its components '''
+    def __init__(self, filename):
+        self.filename = filename
+        self._rooms = {}
+        self._doors = []
+        self._items = {}
+        fileParser(filename)
 
     def fileParser(self, myFile):
-        """ Parses the config file to store data in dictionaries """
-        print("Hello gitHub!")
+        ''' Parses the config file to store data in dictionaries '''
+        with open(myFile) as f:
+            lines = f.readlines()
+
+        # Desired result:
+        # bedroom = Room(self._doors, self._items)
+        # + Items/Doors instanciated ??
 
 
 class Game:
-    """ Main class of the game """
+    ''' Main class of the game '''
     def __init__(self, house, character):
         self.house = house
         self.character = character
