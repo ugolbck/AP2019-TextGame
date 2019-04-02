987e1ec5e081888f34fda01a8040f1853d5b1e6f
Ugo Loobuyck <ugo813@hotmail.fr>
2019-04-02 11:32:18 +0200

    "show" method completed. "cardinals" in Command init.

--- textGame.py
+++ textGame.py
@@ -27,10 +27,10 @@ class Character:
 
 class House:
     ''' Class that defines the house and holds its components '''
-    N_WALL = 'N'
-    S_WALL = 'S'
-    E_WALL = 'E'
-    W_WALL = 'W'
+    N = 'N'
+    S = 'S'
+    E = 'E'
+    W = 'W'
     ITEMS = 'itemlist'
 
     def __init__(self, filename):
@@ -47,17 +47,17 @@ class House:
             split = line.split(' ')
             if split[0] == 'room':
                 self._house_map[split[1][:-1]] = {
-                                            self.N_WALL: '',
-                                            self.S_WALL: '',
-                                            self.E_WALL: '',
-                                            self.W_WALL: '',
+                                            self.N: '',
+                                            self.S: '',
+                                            self.E: '',
+                                            self.W: '',
                                             self.ITEMS: {}
                                             }
             elif split[0] == 'door':
                 self._house_map[split[3]][split[1][-1]] = [split[4][:-1], split[2]]
                 self._house_map[split[4][:-1]][split[1][0]] = [split[3], split[2]] 
             elif split[0] == 'item':
-                self._house_map[split[2]][self.ITEMS][split[1]] = (split[3], split[4][:-1]) if len(split) == 5 else split[3][:-1]
+                self._house_map[split[2]][self.ITEMS][split[1]] = (split[3], split[4][:-1]) if len(split) == 5 else (split[3][:-1], '')
             elif split[0] == 'start':
                 self._position = line.split(' ')[1]
 
@@ -71,10 +71,17 @@ class House:
 class Commands:
     ''' Set of available user commands '''
     def __init__(self):
-        pass
-    
-    def show(self):
-        pass
+        self.cardinals = ['N', 'S', 'E', 'W']
+        
+    def show(self, position, house_map):
+        ''' Void method that shows the player his environment '''
+        available_rooms = [key for key, val in house_map[position].items() if val and key in self.cardinals]
+        available_items = [key for key in house_map[position]['itemlist'].keys()]
+        print('You are now in the', position + '.')
+        print('Available doors:', ', '.join(available_rooms)) if available_rooms else print('There are no doors. You are basically in jail.')
+        print('Available items:', ', '.join(available_items)) if available_items else print('There are no useful items in this room.')
+        
+        
 
 
 class Game:
@@ -87,23 +94,25 @@ class Game:
     def play(self):
         self.title()
         self.character.setPosition(self.house.getPosition())
-        
+        self.command.show(self.character.getPosition(), self.house.getRoomMap())
+        # print(self.character.getPosition())
+        # print(self.house.getRoomMap()['LivingRoom'][self.house.N])
 
     def title(self):
         print('#######################################################')
-        time.sleep(0.4)
+        # time.sleep(0.4)
         print('################   Welcome to DEFUSE   ################')
-        time.sleep(0.4)
+        # time.sleep(0.4)
         print('#######################################################')
-        time.sleep(0.8)
+        # time.sleep(0.8)
         print('###########        Ugo Loobuyck 2019        ###########')
-        time.sleep(0.3)
+        # time.sleep(0.3)
         print('#######           github.com/ugolbck/           #######')
-        time.sleep(0.3)
+        # time.sleep(0.3)
         print('###   Project: github.com/ugolbck/AP2019-TextGame   ###')
-        time.sleep(0.3)
+        # time.sleep(0.3)
         print('#                                                     #')
-        time.sleep(0.3)
+        # time.sleep(0.3)
         print()
         print()
 
